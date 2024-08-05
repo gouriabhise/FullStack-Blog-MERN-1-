e00dc2bb70204fb40ba7b5de9b50ac6632a54310
gouribhise <gouri.a.bhise@gmail.com>
2024-08-05 13:43:34 +0530

    added login auth

--- frontend/src/pages/users/Login.jsx
+++ frontend/src/pages/users/Login.jsx
@@ -1,24 +1,49 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useLoginUserMutation } from '../../redux/features/Auth/authApi'
 
 const Login = () => {
     const[email,setEmail]=useState('')
     const[password,setPassowrd]=useState('')
     const[message,setMessage]=useState('')
+const[loginUser,{isLoading:loginLoading}]=useLoginUserMutation()
+const navigate=useNavigate()  
+const handleLogin=async(e)=>{
+e.preventDefault()
+const data={
+  email,
+  password
+}
+//console.log(data)
+try{
+const response=await loginUser(data).unwrap()
+console.log(response)
+const{token,user}=response
+alert("Login Successfull!")
+navigate('/')
+}catch(error){
+
+}
+    }
   return (
     <div className='max-w-sm bg-white mx-auto p-8 mt-36'>
       <h1 className='text-2xl font-semibold pt-5'>Please Login</h1>
-      <form className='space-y-5 max-w-sm mx-auto pt-8'>
+      <form onSubmit={handleLogin} className='space-y-5 max-w-sm mx-auto pt-8'>
 <input type='text' value={email} className='w-full bg-white  focus:outline-none px-5 py-3'
 placeholder='Email'
-required/>
+required
+onChange={(e)=>setEmail(e.target.value)}
+/>
 <input type='password' value={password} className='w-full bg-white  focus:outline-none px-5 py-3'
 placeholder='Password'
+onChange={(e)=>setPassowrd(e.target.value)}
 required/>
 {
     message && <p className='text-red-500'>{message}</p>
 }
-<button className='w-full mt-5 bg-black hover:bg-indigo-500 text-white font-medium rounded-md'>Login</button>
+<button 
+disabled={loginLoading}
+className='w-full mt-5 bg-black hover:bg-indigo-500 text-white font-medium rounded-md'>Login</button>
       </form>
       <p className='my-5 text-center'>Don't have an account? <Link to="/register" className="text-red-700">Register</Link></p>
     </div>
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { blogsApi } from "./features/Blogs/blogsApi";
-
+import authApi from "./features/Auth/authApi";
+import authReducer from './features/Auth/authSlice'
 export const store=configureStore({
     reducer:{
-        [blogsApi.reducerPath]:blogsApi.reducer
+        [blogsApi.reducerPath]:blogsApi.reducer,
+        [authApi.reducerPath]:authApi.reducer,
+        auth:authReducer,
     },
-    middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(blogsApi.middleware)
+    middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(blogsApi.middleware,authApi.middleware)
 })
